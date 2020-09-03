6820d4f95cd60dc17c3521be45dccc7d0cb9b8af
燕宗振 <yanzongzhen@boommeeting.com>
2020-09-03 11:46:47 +0800

    merge new sctp master to listener and fix some bugs

--- server/client.go
+++ server/client.go
@@ -1066,7 +1066,7 @@ func (c *client) readLoop(pre []byte) {
 			b = make([]byte, c.in.rsz)
 		} else if n < cap(b) && cap(b) > minBufSize && c.in.srs > shortsToShrink {
 			// Shrink, for now don't accelerate, ping/pong will eventually sort it out.
-			//c.in.rsz = int32(cap(b) * (2 / 3))
+			//c.in.rsz = int32(cap(b) / 2)
 			//b = make([]byte, c.in.rsz)
 		}
 		c.mu.Unlock()
